129e22b520fab1c6fa4e05b3cdc7add7cf18cef2
Priya Kumari <priyakumari21145@gmail.com>
2025-11-21 01:04:42 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,11 @@
+import math
+num = float(input("Enter a number: "))
+
+
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+
+print(f"Square root of {num} is: {square_root}")
+print(f"Natural logarithm (log base e) of {num} is: {natural_log}")
+print(f"Sine of {num} (in radians) is: {sine_value}")
